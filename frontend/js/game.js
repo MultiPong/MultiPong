f450785a7cd118270123070e7ae670356e44a131
Bingo <55219554+thebingobongo@users.noreply.github.com>
2023-11-01 00:21:56 -0400

    basic game state with loaded sprites and keyboard input

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -1,10 +1,10 @@
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     physics: {
-      default: 'matter', 
+      default: 'arcade', 
       //arcade: better for performance but 90 degree only
       arcade: {
         debug: false,
@@ -26,27 +26,32 @@ var config = {
 
   //generally used to load assets
   function preload() {
-    this.load.image('wall');
-    this.load.image('ball');
+    this.load.image('paddle','js/assets/sprites/wall.png');
+    this.load.image('ball','js/assets/sprites/ball.png');
   }
 
   //initialize game
   function create() {
-    num_players = 4;
-    theta = 360/num_players;
-    Radius = 1000;
-    for(let i = 0; i < num_players; i++){
-        //TODO: implement angle-render func in desmos (with offsets) for paddles and borders
-        x = X(i*theta, Radius);
-        this.matter.add.image(/*x*/x,/*y*/(x*Math.tan(i*theta*Math.PI/180)),/*sprite*/'wall',null,{isStatic: true}).setScale(/*length*/1,/*thickness*/0.5).setAngle((i*theta)+90);
-    }
-    ball = this.matter.add.image(0,0, /*ball sprite name*/'ball');
-    ball.setScale(0.5,0.5);
-    ball.setCircle();
-    ball.setBounce(1);
-    randangle = Phaser.Math.Between(0,359)*Math.PI/180;
-    spd = 10;
-    ball.setVelocity(spd*Math.cos(randangle), spd*Math.sin(randangle));
+    cursors = this.input.keyboard.createCursorKeys();
+    player = this.physics.add.sprite(100, 100, 'ball');
+    platforms = this.physics.add.staticGroup();
+    platforms.create(500,500, 'paddle').setScale(2).refreshBody();
+
+    // num_players = 4;
+    // theta = 360/num_players;
+    // Radius = 1000;
+    // for(let i = 0; i < num_players; i++){
+    //     //TODO: implement angle-render func in desmos (with offsets) for paddles and borders
+    //     x = X(i*theta, Radius);
+    //     this.matter.add.image(/*x*/x,/*y*/(x*Math.tan(i*theta*Math.PI/180)),/*sprite*/'wall',null,{isStatic: true}).setScale(/*length*/1,/*thickness*/0.5).setAngle((i*theta)+90);
+    // }
+    // ball = this.matter.add.image(0,0, /*ball sprite name*/'ball');
+    // ball.setScale(0.5,0.5);
+    // ball.setCircle();
+    // ball.setBounce(1);
+    // randangle = Phaser.Math.Between(0,359)*Math.PI/180;
+    // spd = 10;
+    // ball.setVelocity(spd*Math.cos(randangle), spd*Math.sin(randangle));
 
 
 
@@ -55,6 +60,29 @@ var config = {
   //Code that happens during runtime
   function update() {
     //TODO: paddle inputs, powerups*, time-based mechanics*
+      if (cursors.left.isDown)
+      {
+          player.setVelocityX(-160);
+
+          // player.anims.play('left', true);
+      }
+      else if (cursors.right.isDown)
+      {
+          player.setVelocityX(160);
+
+          // player.anims.play('right', true);
+      }
+      else
+      {
+          player.setVelocityX(0);
+
+          // player.anims.play('turn');
+      }
+
+      if (cursors.up.isDown && player.body.touching.down)
+      {
+          player.setVelocityY(-330);
+      }
   }
 
 function X(angle, radius){
