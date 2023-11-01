7b8bbec4fe9902de72820b91d30f0a5b8931e514
Anhr13 <101531867+Anhr13@users.noreply.github.com>
2023-10-31 21:14:08 -0400

    Update game.js

---
+++ frontend/js/game.js
@@ -0,0 +1,28 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 0 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  } 
+};
+ 
+var game = new Phaser.Game(config);
+ 
+function preload() {}
+ 
+function create() {
+  this.socket = io();
+}
+ 
+function update() {}
