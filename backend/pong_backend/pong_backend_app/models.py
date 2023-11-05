4963a2dc138821862a5fbe1350125044da392a20
Brandon Ly <github@brndn.ly>
2023-11-05 17:01:20 -0500

    Merge pull request #4
    * chore: setup django install and python requirements
    
    * feat: create basic models
    
    * refactor: switch ID's to using UUID4

--- backend/pong_backend/pong_backend_app/models.py
+++ backend/pong_backend/pong_backend_app/models.py
@@ -1,3 +1,41 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    userID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    matches = models.ManyToManyField('Match', through='PlayerMatchRelation', related_name='players')
+
+    def __str__(self):
+        return self.username
+
+
+class Match(models.Model):
+    matchID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    startTime = models.DateTimeField(null=True)
+    endTime = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return str(self.matchID)
+
+
+class PlayerMatchRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    timeAlive = models.IntegerField(default=0, db_index=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['timeAlive'], name='timeAlive_idx')
+        ]
+        unique_together = ('user', 'match')
+
+
+
+
+
