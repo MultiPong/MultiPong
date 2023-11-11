56a8b84f6d5683e9c06ac53e775694e6e418d522
Brandon Ly <github@brndn.ly>
2023-11-10 21:24:47 -0500

    update backend-dev (#11)
    * test
    
    * Added Game Creation, Game Lobby and User Profile
    
    ---------
    
    Co-authored-by: Ashik Reji <ashikreji27@gmail.com>
    Co-authored-by: Joe3249 <ashweenvivek@gmail.com>

---
+++ frontend/src/components/GameCreation.jsx
@@ -0,0 +1,64 @@
+
+import React, { useState } from 'react';
+import './Stuff.css'
+/*import GameLobby from './GameLobby.jsx';*/
+
+function GameCreation({changeState}) {
+  
+
+  
+  const [numPlayers, setNumPlayers] = useState(2)
+  const [ballSpeed, setBallSpeed] = useState(1.5)
+  return (
+    <div className="home-container">
+      <div className='Game-Creation-box'>
+      <h1>MULTIPONG</h1>
+        <div className='inner-Game-Creation-box'>
+          <div>
+            <input
+                type="range"
+                min={2}
+                max={8}
+                step={2}
+                value={numPlayers}
+                onChange={event => {
+                  setNumPlayers(event.target.valueAsNumber)
+                }}
+              />
+              <p>Number of players: {numPlayers.toFixed(0)}</p>
+
+              <input
+                type="range"
+                min={1.5}
+                max={3}
+                step={0.5}
+                value={ballSpeed}
+                onChange={event => {
+                  setBallSpeed(event.target.valueAsNumber)
+                }}
+              />
+              <p>Ball Speed: {ballSpeed.toFixed(1)}</p>
+              <button className="create-game-button">
+                CREATE GAME
+              </button>
+          </div>
+          <div className='stuff-box'>
+              <input className='input' type="text" placeholder="Enter Code" />
+              <button className="join-game-button">
+                JOIN GAME
+              </button>
+              <div>
+                <button className="back-button">
+                  Back
+                </button>
+              </div>
+            
+          </div>
+        </div>
+        
+        
+      </div>
+    </div>
+  );
+}
+export default GameCreation;
