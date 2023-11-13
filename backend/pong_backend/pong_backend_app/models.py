decefdfccbea762ab52722a0afb58c4a29414285
Brandon Ly <git@brndn.ly>
2023-11-13 03:36:52 -0500

    feat: adapted the user model to django user model

--- backend/pong_backend/pong_backend_app/models.py
+++ backend/pong_backend/pong_backend_app/models.py
@@ -1,18 +1,58 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 import uuid
 from django.db import models
 
 # Create your models here.
 
 
-class User(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError("Users must have a username")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(
+            email='none',
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     userID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    email = models.EmailField(verbose_name='email', max_length=100, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     matches = models.ManyToManyField('Match', through='PlayerMatchRelation', related_name='players')
+
+    USERNAME_FIELD = 'username'
+
+    objects = UserManager()
 
     def __str__(self):
         return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
 
 
 class Match(models.Model):
