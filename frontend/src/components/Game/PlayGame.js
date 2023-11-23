de6b927325937898cf692b3a13eed70b127d6522
Adam Azizi <adamazizi101@gmail.com>
2023-11-23 04:01:30 -0500

    part 2

--- frontend/src/components/Game/PlayGame.js
+++ frontend/src/components/Game/PlayGame.js
@@ -8,7 +8,7 @@ function PlayGame({ authToken, gameRoomID }) {
       <h1>Auth Token is: {authToken}</h1>
       
 
-<div style={{ position: 'relative', borderRadius: '25px' }} className="home-container-Play">
+<div style={{ position: 'relative' }} className="home-container-Play">
       
       <div style={{ zIndex: 1, position: 'absolute' }} className='Game-Creation-box-iframe'>
 
@@ -16,7 +16,8 @@ function PlayGame({ authToken, gameRoomID }) {
           
         <iframe
         title={`Game Room ${gameRoomID}`}
-        src={`http://127.0.0.1:5500/index.html?id=${gameRoomID}&${authToken}`}
+        src={`http://localhost:8081/`}
+        // src={`http://127.0.0.1:5500/index.html?id=${gameRoomID}&${authToken}`}
         width="800"
         height="600"
         frameBorder="0"
