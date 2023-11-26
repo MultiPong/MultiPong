2dd6994ae93bcca9bd41c8e207bc2b378f45b62d
Bingo <55219554+thebingobongo@users.noreply.github.com>
2023-11-26 06:31:52 -0500

    combined backend, initial tests with game are working

--- backend/pong_backend/pong_backend/routing.py
+++ backend/pong_backend/pong_backend/routing.py
@@ -1,4 +1,5 @@
 from django.urls import re_path
+from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -9,7 +10,9 @@ websocket_urlpatterns = [
 ]
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
+    # Handle HTTP requests through Django's ASGI application
+    "http": get_asgi_application(),
+    # Handle WebSocket requests through Channels' URLRouter
     'websocket': AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
